a37a340456ba4ac5dc94b23f82ce26117a461655
Snailzzj <1456871731@qq.com>
2023-10-10 10:50:54 +0800

    使用iframe防止浏览器拦截下载

--- src/saveFileToBlob.js
+++ src/saveFileToBlob.js
@@ -7,19 +7,38 @@ import isBlob from 'is-blob';
  * @param fileType  ：文件类 word(docx) excel(xlsx) ppt等
  */
 export default function saveFileToBlob(blob, tagFileName, fileType) {
-  if (fileType) {
-    console.warn('未传入文件类型,例如文件类 word(docx) excel(xlsx) ppt jpg png等')
+  if (!fileType) {
+    console.warn(
+      "未传入文件类型,例如文件类 word(docx) excel(xlsx) ppt jpg png等"
+    );
   }
   if (isBlob(blob)) {
-    var downloadElement = document.createElement('a');
+    var hiddenIFrameID = "hiddenDownloader" + count++;
+    var iframe = document.createElement("iframe");
+    iframe.id = hiddenIFrameID;
+    iframe.style.display = "none";
+    document.body.appendChild(iframe);
+
     let href = window.URL.createObjectURL(blob); //创建下载的链接
-    downloadElement.href = href;
-    downloadElement.download = (tagFileName ? tagFileName : moment(new Date().getTime()).format('YYYYMMDDhhmmss')) + '.' + fileType; //下载后文件名
-    document.body.appendChild(downloadElement);
-    downloadElement.click(); //点击下载
-    document.body.removeChild(downloadElement); //下载完成移除元素
+    iframe.src = href;
+
+    // var downloadElement = document.createElement("a");
+    // let href = window.URL.createObjectURL(blob); //创建下载的链接
+    // downloadElement.href = href;
+    // downloadElement.download =
+    //   (tagFileName
+    //     ? tagFileName
+    //     : moment(new Date().getTime()).format("YYYYMMDDhhmmss")) +
+    //   "." +
+    //   fileType; //下载后文件名
+    // document.body.appendChild(downloadElement);
+    // downloadElement.click(); //点击下载
+    // document.body.removeChild(downloadElement); //下载完成移除元素
+
     window.URL.revokeObjectURL(href); //释放掉blob对象
   } else {
-    console.warn('不是blob对象类型的参数,可选择saveFileToBase64(Base64对象,文件类型)或saveFileToLink(文件链接,文件名,文件类型,进度回调方法)')
+    console.warn(
+      "不是blob对象类型的参数,可选择saveFileToBase64(Base64对象,文件类型)或saveFileToLink(文件链接,文件名,文件类型,进度回调方法)"
+    );
   }
 }
